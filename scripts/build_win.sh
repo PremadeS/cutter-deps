402e5ed770a4f3f457b129ff13a7aa9a40ca0b36
PremadeS <128969155+PremadeS@users.noreply.github.com>
2026-04-26 14:11:26 +0500

    windows llvm

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -1,6 +1,6 @@
 set -euo pipefail
 
-LLVM_NAME=clang+llvm-20.1.8-x86_64-pc-windows-msvc.tar.xz 
+LLVM_NAME=clang+llvm-20.1.8-x86_64-pc-windows-msvc
 LLVM_ARCHIVE="$LLVM_NAME.tar.xz"
 wget --progress=dot:giga https://github.com/llvm/llvm-project/releases/download/llvmorg-20.1.8/$LLVM_ARCHIVE
 echo "f229769f11d6a6edc8ada599c0cda964b7dee6ab1a08c6cf9dd7f513e85b107f  ./$LLVM_ARCHIVE" | sha256sum -c -
